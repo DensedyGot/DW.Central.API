3d3f48c458dfb01375c89bb4abccf0b9efa78771
Densedy Isaguirre <densedy.isaguirre@puma.com>
2024-10-21 17:28:51 +0800

    Refactor token acquisition URLs and scopes
    Modified FlowMonitoring.cs to use dynamic URL from environmentUrls array for access token acquisition, enhancing flexibility. Updated TokenService.cs to use a hardcoded scope for token acquisition, simplifying the process and making the scope explicit.

--- DW.Central.API/Services/MicrosoftEntra/TokenService.cs
+++ DW.Central.API/Services/MicrosoftEntra/TokenService.cs
@@ -41,7 +41,8 @@ namespace DW.Central.API.Services.MicrosoftEntra
                         .WithCertificate(certificate)
                         .Build();
                 logger.LogInformation($"FlowMonitoring > TokenService.cs = GetTokenFromCertificateAsync > Step 3 > {azureApp.Authority}");
-                AuthenticationResult authPromise = azureApp.AcquireTokenForClient(scopes).ExecuteAsync().GetAwaiter().GetResult();
+                //AuthenticationResult authPromise = azureApp.AcquireTokenForClient(scopes).ExecuteAsync().GetAwaiter().GetResult();
+                AuthenticationResult authPromise = azureApp.AcquireTokenForClient(new string[] { "https://management.azure.com/.default" }).ExecuteAsync().GetAwaiter().GetResult();
                 logger.LogInformation($"FlowMonitoring > TokenService.cs = GetTokenFromCertificateAsync > Step 4 > {authPromise.TenantId}");
                 return authPromise.AccessToken;
             }
